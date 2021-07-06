90649650977ac94952758631bbc428389ac14576
ASPECT30 <87013929+ASPECT30@users.noreply.github.com>
2021-07-07 06:38:54 +0800

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,7 +6,7 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.entod(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(f"`{ALIVE_NAME} Berusaha Menentod Jamet Ini`")
+    await typew.edit(f"`{ALIVE_NAME} Berusaha Menentod Lonte Ini`")
     sleep(2)
     await typew.edit("`Mencari Nama Lonte Ini...`")
     sleep(1)
